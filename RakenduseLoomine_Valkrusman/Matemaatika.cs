9514077d465791feadaea45b7bad42cf1bf36c4e
opilane <opilane@HP-CZC6297B89>
2022-10-04 12:58:21 +0300

    Add project files.

---
+++ RakenduseLoomine_Valkrusman/Matemaatika.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Label = System.Windows.Forms.Label;
+
+namespace RakenduseLoomine_Valkrusman
+{
+    public partial class Matemaatika : Form
+    {
+        Label timeLabel;
+        TableLayoutPanel tableLayoutPanel;
+        NumericUpDown[] vastused = new NumericUpDown[4];//{ summa, lahutamine, jagamine, korrutamine };
+        string[,] l_nimed;
+        string text;
+        string[] tehed = new string[4] { "+", "-", "/", "*" };
+        Random random = new Random(10);
+        Timer timer = new Timer { Interval = 1000 };
+        int[] num1=new int[4];
+        int[]num2=new int[4];
+        
+
+        public Matemaatika()
+        {
+
+            this.Size = new Size(680, 400);
+
+            this.Size = new Size(600, 500);
+
+            tableLayoutPanel = new TableLayoutPanel
+            {
+                AutoSize = true,
+                ColumnCount = 5,
+                RowCount = 4,
+
+                Location = new System.Drawing.Point(50, 60),
+                BackColor = System.Drawing.Color.Cyan
+
+               
+            };
+            timeLabel = new Label
+            {
+                Text = "Aega veel",
+                AutoSize = false,
+                BorderStyle = BorderStyle.FixedSingle,
+                Size = new Size(200, 30),
+                Font = new Font("Arial", 24, FontStyle.Bold),
+
+
+            };
+            string[,] l_nimed = new string[5, 4];
+            timer.Enabled = true;
+            timer.Tick += Timer_Tick;
+            this.DoubleClick += Matemaatika_DoubleClick1;
+            
+            for (int i = 0; i < 4; i++)
+            {
+                tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25f));
+                for (int j = 0; j < 5; j++)
+                {
+                    tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20f));
+                    var l_nimi = "L" + j.ToString() + i.ToString() ;
+                    l_nimed[j, i] = l_nimi;
+                    if (j == 1) { text = tehed[i]; }
+                    else if (j == 3) { text = "="; }
+                    else if(j== 4) { text = "vastus"; }
+                    else if (j == 0)
+                    {
+                        int a = random.Next(10);
+                        text = a.ToString();
+                        num1[i] = a;
+                        
+                    }
+                    else if (j == 2)
+                    {
+                        int a = random.Next(10);
+                        text = a.ToString();
+                        num2[i] =a;
+                    }
+                     if (j == 4)
+                    {
+                        vastused[i] = new NumericUpDown 
+                        {
+                            Name = tehed[i],
+                            DecimalPlaces = 2,
+                            Minimum=-10
+                        };
+                        tableLayoutPanel.Controls.Add(vastused[i], j, i);
+                    }
+                    else
+                    {
+                        Label l = new Label { Text = text };
+                        tableLayoutPanel.Controls.Add(l, j, i);
+                    }
+                    
+                }
+
+
+            }
+            this.Controls.Add(tableLayoutPanel);
+            this.Controls.Add(timeLabel);
+
+             
+            
+           
+            for (int i = 0; i< 4; i++)
+            {
+                tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25f));
+                for (int j= 0; j< 5; j++)
+                {
+                    tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20f));
+                    var l_nimi = "L" + j.ToString() + i.ToString() + i.ToString();
+                    l_nimed[j, i] = l_nimi;
+                    if (j==1)
+                    {
+                        Text = tehed[i];
+                    }
+                    else if (j==3)
+                    {
+                        Text = "=";
+                    }
+                    else if (j == 4)
+                     {
+                 Text = "vastus";
+                    }
+                    else
+                    {
+                    Text = l_nimi;
+                    }
+                Label l = new Label { Text = text };
+                tableLayoutPanel.Controls.Add(l, j, i);
+                }
+
+
+            }
+
+
+
+
+        }
+        int tik = 0;
+
+        private void Matemaatika_DoubleClick1(object sender, EventArgs e)
+        {
+            timer.Start();
+            timeLabel.Text = timer.ToString();
+            timeLabel.Location = new Point(300, 300);
+            tableLayoutPanel.Controls.Add(timeLabel);
+        }
+
+        
+        private bool Kontroll()
+        {
+            if (num1[0] + num2[0] == vastused[0].Value 
+                && num1[1] - num2[1] == vastused[1].Value 
+                && num1[2] / num2[2] == vastused[2].Value 
+                && num1[3] * num2[3] == vastused[3].Value)
+            {
+                return true;
+
+            }
+            else { return false; }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tik++;
+            timeLabel.Text = tik.ToString();
+            if (Kontroll())
+            {
+                timer.Stop();
+                MessageBox.Show("Palju õnne!", "sinu vastused on õiged");
+            }
+        }
+    }
+}
---
+++ RakenduseLoomine_Valkrusman/PildidApp.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RakenduseLoomine_Valkrusman
+{
+
+    public class PildidApp : Form1
+    {
+        TableLayoutPanel tableLayoutPanel;
+        FlowLayoutPanel flowLayoutPanel;
+        PictureBox pictureBox;
+        PictureBox pilt;
+        CheckBox checkBox;
+        Button clear_btn;
+        Button show_btn;
+        Button close_btn;
+        OpenFileDialog openFileDialog;
+        int x = 150;
+        int y = 450;
+
+        public PildidApp()
+        {
+            this.Text = "Pildid";
+            this.ClientSize = new System.Drawing.Size(900, 500);
+            this.tableLayoutPanel = new TableLayoutPanel
+            {
+                AutoSize = true,
+                Dock = DockStyle.Fill,
+                ColumnCount = 2,
+                RowCount = 2,
+
+                Location = new System.Drawing.Point(20, 0),
+                Size =new System.Drawing.Size(300, 400),
+
+                
+
+                TabIndex = 0,
+                BackColor = System.Drawing.Color.White,
+            };
+
+            this.Controls.Add(tableLayoutPanel);
+
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+                (System.Windows.Forms.SizeType.Percent, 15f));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle
+                (System.Windows.Forms.SizeType.Percent, 85f));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.RowStyle
+                (System.Windows.Forms.SizeType.Percent, 90f));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.RowStyle
+                (System.Windows.Forms.SizeType.Percent, 10f));
+            this.tableLayoutPanel.ResumeLayout(false);
+
+
+            //this.pilt = new PictureBox
+            //{
+            //    Image = new Bitmap(@"..\..\france.jpg"),
+            //    Location = new Point(x, y),
+            //    Size = new Size(100, 100),
+            //    SizeMode = PictureBoxSizeMode.Zoom
+            //};
+
+
+            this.pilt = new PictureBox
+            {
+                Image = new Bitmap(@"..\..\france.jpg"),
+                Location = new Point(x, y),
+                Size = new Size(100, 100),
+                SizeMode = PictureBoxSizeMode.Zoom
+            };
+
+            this.tableLayoutPanel.Controls.Add(pilt, 10, 0);
+            this.tableLayoutPanel.SetCellPosition(pilt, new TableLayoutPanelCellPosition(0, 0));
+            this.tableLayoutPanel.SetColumnSpan(pilt, 2);
+
+            this.pictureBox = new System.Windows.Forms.PictureBox
+            {
+
+                Image = new Bitmap(@"..\..\france.jpg"),
+
+                BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TabIndex = 0,
+                TabStop = false,
+            };
+            this.tableLayoutPanel.Controls.Add(pictureBox, 0, 0);
+            this.tableLayoutPanel.SetCellPosition(pictureBox, new TableLayoutPanelCellPosition(0, 0));
+            this.tableLayoutPanel.SetColumnSpan(pictureBox, 2);
+
+
+            this.checkBox = new CheckBox
+            {
+                Text = "Venita",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+            };
+            this.tableLayoutPanel.Controls.Add(checkBox, 1, 0);
+            this.Controls.Add(tableLayoutPanel);
+
+
+            this.close_btn = new Button
+            {
+                Text = "Pane Kinni",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+            };
+            this.clear_btn = new Button
+            {
+                Text = "Näita",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+            };
+            this.show_btn = new Button
+            {
+                Text = "Näita",
+                Dock = System.Windows.Forms.DockStyle.Fill,
+            };
+
+            this.show_btn.Click += Tegevus;
+            this.clear_btn.Click += Tegevus;
+            this.close_btn.Click += Tegevus;
+            Button[] buttons = { clear_btn, show_btn, close_btn };
+            this.flowLayoutPanel = new FlowLayoutPanel
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                FlowDirection = FlowDirection.RightToLeft,
+                AutoSize = true,
+                WrapContents = false,
+                AutoScroll = true,
+                Location = new System.Drawing.Point(0, 0),
+                Size = new System.Drawing.Size(300, 50),
+            };
+            foreach (Button button in buttons)
+            {
+                this.flowLayoutPanel.Controls.Add(button);
+            }
+            this.tableLayoutPanel.Controls.Add(flowLayoutPanel, 1, 0);
+
+        }
+
+        private void Tegevus(object sender, EventArgs e)
+        {
+            Button nupp_sender = (Button)sender;
+            if (nupp_sender.Text == "Näita")
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox.Load(openFileDialog.FileName);
+                }
+
+            }
+            else if (nupp_sender.Text == "kustuta")
+            {
+                pictureBox.Image = null;
+            }
+            else if (nupp_sender.Text == "kinni")
+            {
+                this.Close();
+            }
+        }
+    }
+}
+
